3c6b0f824ef96e40db57eed965a5ee034623cceb
David Souther <davidsouther@gmail.com>
2012-09-09 15:44:28 -0400

    Added templates and first round of feed rendering; switched to JSON feed.

--- js/jqbp.js
+++ js/jqbp.js
@@ -1,7 +1,19 @@
 jQuery(function($){
 
-	$.ajax('newswire.php', {dataType: "xml"}).then(function(data){
-		console.log(data);
+	$.getJSON('newswire.php').then(function(data){
+		if(data.status === 'OK') {
+			$.each(data.results, showResult);
+		}
 	});
+
+	function showResult(i, result){
+		var created = new Date(result.created_date);
+		var $template = $($("#templates #news_item").clone());
+		$template.find(".title a").text(result.title).attr("href", result.url);
+		$template.find(".byline").text(result.byline);
+		$template.find(".abstract").text(result.abstract);
+		$template.find(".created").text(created.toLocaleString());
+		$template.appendTo("#firehose");
+	}
 
 }(jQuery));
